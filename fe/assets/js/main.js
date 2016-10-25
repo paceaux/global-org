4f26e84490d4b8faa2f9cb04efefe7f0b7ff4220
Frank Taylor <frank@tahzoo.com>
2016-10-25 18:12:40 -0500

    sets react to render the table, provide for deletions, also changes position of id to first, so it matches with table

--- fe/assets/js/main.js
+++ fe/assets/js/main.js
@@ -1,59 +1,152 @@
+function jsonToQueryString(json) {
+    return '?' +
+        Object.keys(json).map(function(key) {
+            return encodeURIComponent(key) + '=' +
+                encodeURIComponent(json[key]);
+        }).join('&');
+}
 
-
-var FieldName = React.createClass({
-  render: function() {
-    return (
-      <th className="table__cell table__cell--head">
-      </th>
-    );
-  }
-});
-
-var TableHead = React.createClass({
-	render: function () {
-		var fieldHeadings = this.props.data.map(function(field) {
-			return (
-				<th id={field.fieldName}>
-					{field.fieldName}
-				</th>
-			)
-		});
-
+class EmployeeField extends React.Component {
+	render () {
 		return (
-			<tr className="table__row" data={this.props.data}>
-				{fieldHeadings}
-			</tr>
+			<td className="table__cell" for={data.key}>
+				{data.value}
+			</td>
 		)
 	}
-});
-
-
-var EmployeeField = React.createClass({
-  render: function() {
-    return (
-      <td className="table__cell">
-      </td>
-    );
-  }
-});
-
-var Employee = React.createClass({
-	render: function() {
-	var employeeFields = this.props.data.map(function(field) {
-
-			return (
-				<td for={field.fieldName} >
-					{field.fieldValue}
-				</td>
-			);
-		});
-	return (
-		<tr className="table__row" data={this.props.data}>
-			{employeeFields}
-		</tr>
-	);
+}
+class TableHead extends React.Component {
+	render () {
+		return (
+			<thead class="table__head" id="fields__headings">
+				<tr className="employees__headings table__row" data={this.props.data}>
+					{Object.keys(this.props.data).map((key) => {
+						return (
+							<th id={key}>
+								{key}
+							</th>
+						)
+					})}
+					<td> </td>
+				</tr>
+			</thead>
+		)
 	}
-});
+};
+
+class EmployeeControlsEdit extends React.Component {
+	render () {
+		return (
+			<button className="edit">Edit</button>
+		)
+	}
+}
+
+
+class EmployeeControlsDelete extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+	getInitialState () {
+		return {
+			isDeleted: false
+		}
+	}
+	handleDelete (event) {
+		console.log(this.props.data);
+		let url = app.server.origin + '/employees/remove?_id=' + this.props.data._id;
+		const xhr = new XMLHttpRequest();
+		console.log(url);
+
+		xhr.open('GET', url);
+		xhr.onload = () => {
+			if (xhr.status === 200 && xhr.readyState === 4) {
+				this.setState(JSON.parse(xhr.responseText));
+			} else {
+				console.warn('Request did not work', xhr.status);
+			}
+		}
+		xhr.send();
+	}
+	render () {
+		return (
+			<button className="delete" onClick={this.handleDelete.bind(this)}>Delete</button>
+		)
+	}
+}
+
+class EmployeeControls extends React.Component {
+	render () {
+		console.log(this.props);
+		return (
+			<td className="employee__controls">
+				<EmployeeControlsEdit />
+				<EmployeeControlsDelete data={this.props.data}/>
+			</td>
+		)
+	}
+}
+
+class Employee extends React.Component{
+	render () {
+		return (
+			<tr className="table__row" data={this.props.data}>
+				{
+					Object.keys(this.props.data).map((key) => {
+						return <td headers={key}>{this.props.data[key]}</td>
+
+					})
+				}
+				<EmployeeControls data={this.props.data}/>
+			</tr>
+		);
+	}
+};
+
+class EmployeeList extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {};
+	}
+	componentWillMount () {
+		const _this = this;
+		const xhr = new XMLHttpRequest();
+		let url = app.server.origin + '/employees/get/';
+
+		xhr.open('GET', url);
+		xhr.onload = () => {
+			if (xhr.status === 200 && xhr.readyState === 4) {
+				this.setState(JSON.parse(xhr.responseText));
+			} else {
+				console.warn('Request did not work', xhr.status);
+			}
+		}
+		xhr.send();
+	}
+	render () {
+		return (
+			<tbody className="employees__list" id="employees__list">
+				{
+					 Object.keys(this.state).map((key)=> {
+						return <Employee data={this.state[key]} />
+					})
+				}
+			</tbody>
+		)
+	}
+};
+
+class EmployeeTable extends React.Component{
+	render () {
+		return (
+			<table id="employees" className="table">
+					<TableHead  data={this.props.data}/>
+				<EmployeeList />
+			</table>
+		)
+	}
+};
 
 var app;
 
@@ -61,15 +154,44 @@ app = {
 	selectors : {
 		employeeForm: '.js-employeeForm',
 	},
+	server: {
+		protocol: 'http',
+		hostname: 'localhost',
+		port: 3000,
+		get origin() {
+			let port = this.port != '' ? ':' + this.port : '';
+			return this.protocol + '://' + this.hostname + port;
+		}
+	},
 	FormData: function (form) {
 		let nodes = form.querySelectorAll('[name]')
-		let formData =  [];
+		let formData =  {};
 
 
 		for (var node of nodes) {
-			formData.push({fieldName: node.name,fieldValue: node.value});
+			formData[node.name] =  node.value;
 		}
 		return formData;
+	},
+	getEmployeeData: function () {
+
+	},
+	addEmployeeData: function (employeeData) {
+		const _this = this;
+		const xhr = new XMLHttpRequest();
+		let data = jsonToQueryString(employeeData);
+		let url = this.server.origin + '/employees/add' + data;
+
+		xhr.open('POST', url);
+		xhr.onload = function () {
+			if (xhr.status === 200) {
+				console.info(xhr.responseText);
+			} else {
+				console.warn('Request did not work', xhr.status);
+			}
+		}
+
+		xhr.send();
 	},
 	bindEvts: function () {
 		const employeeForm = document.querySelector(this.selectors.employeeForm);
@@ -79,23 +201,21 @@ app = {
 			let form  = e.target;
 			let formData = this.FormData(form);
 
-			ReactDOM.render(
-				<Employee  data={formData}/>,
-				document.getElementById('employees__list')
-			);
-			//this.submit();
+			console.log('adding formdata', formData);
+			this.addEmployeeData(formData);
+			this.buildTable();
 		});
 	},
-	buildTHead: function () {
+	buildTable: function () {
 		let formData = this.FormData(document.querySelector(this.selectors.employeeForm));
 
-		ReactDOM.render(
-			<TableHead  data={formData}/>,
-			document.getElementById('fields__headings')
+		ReactDOM.render (
+			<EmployeeTable  data={formData}/>,
+			document.getElementById('global-employees')
 		);
 	},
 	init: function () {
-		this.buildTHead();
+		this.buildTable();
 		this.bindEvts();
 	},
 };
